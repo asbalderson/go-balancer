23443849a2b3636f797fa23324bc80118fee2443
Alex Balderson <alexander.balderson@canonical.com>
2025-12-14 20:04:37 -0700

    add a config function for loading from environment

--- backend/internal/config/config_test.go
+++ backend/internal/config/config_test.go
@@ -1,35 +1,100 @@
 package config
 
 import (
+	"os"
 	"testing"
 )
 
-func TestLoadConfig_Success(T *testing.T) {
-	cfg, err := Load("testdata/valid_config.json")
+func TestLoadConfigFile_Success(t *testing.T) {
+	cfg, err := LoadFromFile("testdata/valid_config.json")
 	if err != nil {
-		T.Fatalf("Expected no error, got: %v", err)
+		t.Fatalf("Expected no error, got: %v", err)
 	}
 
 	if cfg.Port != 8080 {
-		T.Errorf("Expected port 8080, got: %d", cfg.Port)
+		t.Errorf("Expected port 8080, got: %d", cfg.Port)
 	}
 
 	if cfg.ServiceName != "test" {
-		T.Errorf("Expected service name 'test', got '%s'", cfg.ServiceName)
+		t.Errorf("Expected service name 'test', got '%s'", cfg.ServiceName)
 	}
 
 }
 
-func TestLoadConfig_FileNotFound(T *testing.T) {
-	_, err := Load("/no/file.json")
+func TestLoadFileConfig_FileNotFound(t *testing.T) {
+	_, err := LoadFromFile("/no/file.json")
 	if err == nil {
-		T.Error("Exepected error for missing file, but we found the file?")
+		t.Error("Expected error for missing file, but we found the file?")
 	}
 }
 
-func TestLoadConfig_InvalidJson(T *testing.T) {
-	_, err := Load("testdata/invalid_config_port_string.json")
+func TestLoadFileConfig_InvalidJson(t *testing.T) {
+	_, err := LoadFromFile("testdata/invalid_config_port_string.json")
 	if err == nil {
-		T.Error("Epected JSON type error, but we loaded anyway?")
+		t.Error("Expected JSON type error, but we loaded anyway?")
+	}
+}
+
+func TestLoadEnvConfig(t *testing.T) {
+	os.Setenv("SERVICE_NAME", "myservice")
+	os.Setenv("SERVICE_PORT", "1234")
+	t.Cleanup(func() {
+		os.Unsetenv("SERVICE_NAME")
+		os.Unsetenv("SERVICE_PORT")
+	})
+
+	cfg, err := LoadFromEnv()
+
+	if err != nil {
+		t.Fatalf("Expected no error, got: %v", err)
+	}
+
+	if cfg.Port != 1234 {
+		t.Errorf("Expected port 1234, got: %d", cfg.Port)
+	}
+
+	if cfg.ServiceName != "myservice" {
+		t.Errorf("Expected service name 'myservice', got '%s'", cfg.ServiceName)
+	}
+}
+
+func TestLoadEnvConfig_NoName(t *testing.T) {
+	os.Setenv("SERVICE_PORT", "1234")
+	t.Cleanup(func() {
+		os.Unsetenv("SERVICE_PORT")
+	})
+
+	_, err := LoadFromEnv()
+
+	if err == nil {
+		t.Fatalf("Loaded config when no SERVICE_NAME was not set")
+	}
+}
+
+func TestLoadEnvConfig_NoPort(t *testing.T) {
+	os.Setenv("SERVICE_NAME", "myservice")
+	t.Cleanup(func() {
+		os.Unsetenv("SERVICE_NAME")
+	})
+
+	_, err := LoadFromEnv()
+
+	if err == nil {
+		t.Fatalf("Loaded config when no SERVICE_PORT was not set")
+	}
+}
+
+func TestLoadEnvConfig_BadPort(t *testing.T) {
+	os.Setenv("SERVICE_NAME", "myservice")
+	os.Setenv("SERVICE_PORT", "1234a")
+	t.Cleanup(func() {
+		os.Unsetenv("SERVICE_NAME")
+		os.Unsetenv("SERVICE_PORT")
+	})
+
+	_, err := LoadFromEnv()
+
+	if err == nil {
+		t.Fatalf("Loaded config when no SERVICE_PORT was not an int")
 	}
 }
